f004f4ea9ac96db66d7591135da73c84ba3a46dd
zaki <zakidjebiri@outlook.com>
2025-02-11 17:23:11 +0100

    ch15

--- lessons/urls.py
+++ lessons/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import LessonListView, LessonDetailView
+from .views import LessonListView, LessonDetailView, SearchResultsListView
 
 urlpatterns = [
     path("", LessonListView.as_view(), name="lesson_list"),
     path("<uuid:pk>", LessonDetailView.as_view(), name="lesson_detail"),
+    path("search/", SearchResultsListView.as_view(), name="search_results"),
 ]
--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView, DetailView
 from .models import Lesson
@@ -16,3 +17,15 @@ class LessonDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = "lessons/lesson_detail.html"
     login_url = "account_login"
     permission_required = "lessons.special_status"
+
+
+class SearchResultsListView(ListView):
+    model = Lesson
+    context_object_name = "lesson_list"
+    template_name = "lessons/search_list.html"
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        return Lesson.objects.filter(
+            Q(title__icontains=query) | Q(title__icontains=query)
+        )
